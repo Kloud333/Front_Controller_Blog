ebd334b7d75fb41a895ac47adfe49b94480cecb2
Kloud333 <leito2005@i.ua>
2017-09-02 03:01:25 +0300

    Change routs variables name

--- resources/views/posts/posts_list.php
+++ resources/views/posts/posts_list.php
@@ -4,14 +4,16 @@
     <div style="border-bottom: 1px solid #007bff; padding: 10px;">
         <a href="<?= \app\core\createUrl('post_by_id', ['id' => $post_item['id']]) ?>">
             <h3><?= $post_item['title'] ?></h3></a>
-        <p>Author: <a href="<?= \app\core\createUrl('post_by_author', ['author' => $post_item['username']]) ?>"> <?= $post_item['username'] ?></a></p>
+        <p>Author: <a
+                    href="<?= \app\core\createUrl('post_by_author', ['criteria' => $post_item['username']]) ?>"> <?= $post_item['username'] ?></a>
+        </p>
         <p><?= $post_item['created_at'] ?></p>
         <? if (strlen($post_item['content']) >= 50) { ?>
             <p><?= substr($post_item['content'], 0, 50) . ' . . .' ?></p>
         <? } else { ?>
             <p><?= $post_item['content'] ?></p>
         <? } ?>
-        <?php if ($app['route']['name'] == 'user_cabinet_page') { ?>
+        <?php if ($app['route']['name'] == 'user_cabinet_page' && $app['user']) { ?>
             <a href="<?= \app\core\createUrl('edit_post', ['num' => $post_item['id']]) ?>">
                 <button class="btn btn-warning" type="submit" value="Edit">Edit</button>
             </a>
@@ -26,13 +28,13 @@
 <div class="container">
     <div class="col-12 text-center" style="padding: 10px 0; font-size: 25px">
         <?php if ($page != 1) { ?>
-            <a href="<?= \app\core\createUrl($app['route']['name'], ['author' => $tag]) . $link ?>page=1">First</a>
+            <a href="<?= \app\core\createUrl($app['route']['name'], ['criteria' => $tag]) . $link ?>page=1">First</a>
         <?php } ?>
         <?php for ($i = 0; $i < $pages; $i++) { ?>
-            <a href="<?= \app\core\createUrl($app['route']['name'], ['author' => $tag]) . $link . 'page=' . ($i + 1) ?>"><?= ($i + 1) ?></a>
+            <a href="<?= \app\core\createUrl($app['route']['name'], ['criteria' => $tag]) . $link . 'page=' . ($i + 1) ?>"><?= ($i + 1) ?></a>
         <?php } ?>
         <?php if ($page != intval($pages)) { ?>
-            <a href="<?= \app\core\createUrl($app['route']['name'], ['author' => $tag]) . $link ?>page=<?= intval($pages) ?>">Last</a>
+            <a href="<?= \app\core\createUrl($app['route']['name'], ['criteria' => $tag]) . $link ?>page=<?= intval($pages) ?>">Last</a>
         <?php } ?>
     </div>
 </div>
