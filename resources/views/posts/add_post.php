f4541a2b560b2c2a583c3616863dfd49ad05fd1b
Kloud333 <leito2005@i.ua>
2017-09-04 04:34:36 +0300

    Delete check for user login

--- resources/views/posts/add_post.php
+++ resources/views/posts/add_post.php
@@ -1,14 +1,14 @@
 <!-- Add Post Area -->
-<?php if ($app['route']['name'] == 'user_cabinet_page' && $app['user']) { ?>
-    <form method="post" action="<?= \app\core\createUrl('add_post') ?>">
-        <div class="form-group">
-            <input class="form-control input-sm chat-input" name="addPostTitle">
-        </div>
-        <div class="form-group">
-            <textarea class="form-control input-sm chat-input" name="addPostText"></textarea>
-        </div>
-        <div class="form-group">
-            <input class="btn btn-primary btn-md btn-block" type="submit" name="submit" value="Add Post">
-        </div>
-    </form>
-<?php } ?>
+<form class="add-post-form" method="post" action="<?= \app\core\createUrl('add_post') ?>">
+    <div class="form-group">
+        <input class="form-control input-sm chat-input" name="addPostTitle">
+    </div>
+    <div class="form-group">
+        <textarea class="form-control input-sm chat-input" name="addPostText"></textarea>
+    </div>
+    <div class="form-group">
+        <input class="btn btn-primary btn-md btn-block" type="submit" name="submit" value="Add Post">
+    </div>
+</form>
+
+<?= $content ?>
