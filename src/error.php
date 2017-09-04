45e41eb6bb15516104cbf4e1b3f1f563833f9e1b
Kloud333 <leito2005@i.ua>
2017-09-04 04:41:42 +0300

    Changing placing Errors functions

--- src/error.php
+++ src/error.php
@@ -4,16 +4,14 @@ namespace app\src\error;
 
 use app\core;
 
-function httpNotFoundError($message = '')
-{
-    return core\renderView(['http_404.php'], [
+function httpNotFoundError($message = '') {
+    return core\renderView(['errors/http_404.php'], [
         'message' => $message
     ]);
 }
 
-function internalServerError($message = '')
-{
-    return core\renderView(['http_500.php'], [
+function internalServerError($message = '') {
+    return core\renderView(['errors/http_500.php'], [
         'message' => $message
     ]);
 }
