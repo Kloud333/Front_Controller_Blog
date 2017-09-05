20bb40c50931b47ed0938a642d646792de2a85fd
Kloud333 <leito2005@i.ua>
2017-09-06 01:47:31 +0300

    Add tests to registration page

--- tests/acceptance/RegistrationPageCest.php
+++ tests/acceptance/RegistrationPageCest.php
@@ -10,9 +10,30 @@ class RegistrationPageCest {
     }
 
     // tests
+    public function successfulRegistration(AcceptanceTester $I) {
+        $I->wantTo('Successful registration');
+        $I->submitForm('.registration-form', ['username' => 'admin', 'email' => 'admin@mail.com', 'password' => 'admin']);
+        $I->see('Username already exists!');
+    }
+
     public function unsuccessfulRegistrationEmptyFields(AcceptanceTester $I) {
         $I->wantTo('Unsuccessful registration - Empty fields');
         $I->submitForm('.registration-form', ['username' => '', 'email' => '', 'password' => '']);
+        $I->see('Not enough parameters!');
+    }
+    public function unsuccessfulRegistrationEmptyUsername(AcceptanceTester $I) {
+        $I->wantTo('Unsuccessful registration - Empty username');
+        $I->submitForm('.registration-form', ['username' => 'user', 'email' => '', 'password' => '']);
+        $I->see('Not enough parameters!');
+    }
+    public function unsuccessfulRegistrationEmptyEmail(AcceptanceTester $I) {
+        $I->wantTo('Unsuccessful registration - Empty email');
+        $I->submitForm('.registration-form', ['username' => '', 'email' => 'mail', 'password' => '']);
+        $I->see('Not enough parameters!');
+    }
+    public function unsuccessfulRegistrationEmptyPassword(AcceptanceTester $I) {
+        $I->wantTo('Unsuccessful registration - Empty password');
+        $I->submitForm('.registration-form', ['username' => '', 'email' => '', 'password' => 'pass']);
         $I->see('Not enough parameters!');
     }
 
@@ -24,7 +45,8 @@ class RegistrationPageCest {
 
     public function unsuccessfulRegistrationExistingUsername(AcceptanceTester $I) {
         $I->wantTo('Unsuccessful registration - Existing Username');
-        $I->submitForm('.registration-form', ['username' => 'admin', 'email' => 'admin@mail.com', 'password' => 'admin']);
+        $I->submitForm('.registration-form', ['username' => 'user', 'email' => 'user@mail.com', 'password' => 'user']);
+        $I->submitForm('.registration-form', ['username' => 'user', 'email' => 'user@mail.com', 'password' => 'user']);
         $I->see('Username already exists!');
     }
 }
