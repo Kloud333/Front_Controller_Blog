1307585d6ab3d998739aa03d5faabd12cd455d3e
Kloud333 <leito2005@i.ua>
2017-09-06 01:42:01 +0300

    Change path to username

--- resources/views/posts/post_by_id.php
+++ resources/views/posts/post_by_id.php
@@ -1,7 +1,7 @@
 <?php foreach ($articles as $post_item) { ?>
     <div style="border-bottom: 1px solid #007bff; padding: 10px;">
         <h3><?= $post_item['title'] ?></h3>
-        <p>Author: <?= $post_item['username'] ?></p>
+        <p>Author: <?= $post_item['user']['username'] ?></p>
         <p><?= $post_item['created_at'] ?></p>
         <p><?= $post_item['content'] ?></p>
     </div>
