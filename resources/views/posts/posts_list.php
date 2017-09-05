a34308683390fb0cc70d948adc00bbcba533a2bf
Kloud333 <leito2005@i.ua>
2017-09-06 01:42:52 +0300

    Change delete form to delete button

--- resources/views/posts/posts_list.php
+++ resources/views/posts/posts_list.php
@@ -3,7 +3,7 @@
         <a href="<?= \app\core\createUrl('post_by_id', ['id' => $post_item['id']]) ?>">
             <h3><?= $post_item['title'] ?></h3></a>
         <p>Author: <a
-                    href="<?= \app\core\createUrl('post_by_author', ['criteria' => $post_item['username']]) ?>"> <?= $post_item['username'] ?></a>
+                    href="<?= \app\core\createUrl('post_by_author', ['criteria' => $post_item['user']['username']]) ?>"> <?= $post_item['user']['username'] ?></a>
         </p>
         <p><?= $post_item['created_at'] ?></p>
         <? if (strlen($post_item['content']) >= 50) { ?>
@@ -13,12 +13,11 @@
         <? } ?>
         <?php if ($app['route']['name'] == 'user_cabinet_page' && $app['user']) { ?>
             <a href="<?= \app\core\createUrl('edit_post', ['num' => $post_item['id']]) ?>">
-                <button class="btn btn-warning" type="submit" value="Edit" name="edit">Edit</button>
+                <button class="btn col-1 btn-warning" type="submit" value="Edit" name="edit">Edit</button>
             </a>
-            <form class="delete-form d-inline-block" method="post" action="<?= \app\core\createUrl('delete_post') ?>">
-                <input type="hidden" name="postId" value="<?= $post_item['id'] ?>">
-                <input class="btn btn-danger" type="submit" name="delete" value="Delete">
-            </form>
+            <a href="<?= \app\core\createUrl('delete_post', ['num' => $post_item['id']]) ?>">
+                <button class="btn col-1 btn-danger" type="submit" value="Delete" name="delete">Delete</button>
+            </a>
         <?php } ?>
     </div>
 <?php } ?>
@@ -27,17 +26,20 @@
     <ul class="pagination justify-content-center">
         <?php if ($page != 1) { ?>
             <li class="page-item>">
-                <a class="page-link" href="<?= \app\core\createUrl($app['route']['name'], ['criteria' => $tag]) . $link ?>page=1">First</a>
+                <a class="page-link"
+                   href="<?= \app\core\createUrl($app['route']['name'], ['criteria' => $tag]) . $link ?>page=1">First</a>
             </li>
         <?php } ?>
         <?php for ($i = 0; $i < $pages; $i++) { ?>
             <li class="page-item <?= $i == $page - 1 ? 'active' : '' ?>">
-                <a class="page-link <?= $i == $page - 1 ? 'active' : '' ?>" href="<?= \app\core\createUrl($app['route']['name'], ['criteria' => $tag]) . $link . 'page=' . ($i + 1) ?>"><?= ($i + 1) ?></a>
+                <a class="page-link <?= $i == $page - 1 ? 'active' : '' ?>"
+                   href="<?= \app\core\createUrl($app['route']['name'], ['criteria' => $tag]) . $link . 'page=' . ($i + 1) ?>"><?= ($i + 1) ?></a>
             </li>
         <?php } ?>
         <?php if ($page != $pages && $pages != 0) { ?>
             <li class="page-item">
-                <a class="page-link" href="<?= \app\core\createUrl($app['route']['name'], ['criteria' => $tag]) . $link ?>page=<?= $pages ?>">Last</a>
+                <a class="page-link"
+                   href="<?= \app\core\createUrl($app['route']['name'], ['criteria' => $tag]) . $link ?>page=<?= $pages ?>">Last</a>
             </li>
         <?php } ?>
     </ul>
