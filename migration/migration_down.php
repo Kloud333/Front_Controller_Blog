54a1ee66eab2a522ed312d70c76ac90ed3a94339
Kloud333 <leito2005@i.ua>
2017-09-06 01:40:58 +0300

    Rename migration files

--- migration/migration_down.php
+++ migration/migration_down.php
@@ -5,7 +5,8 @@ use Illuminate\Database\Capsule\Manager as Capsule;
 
 try {
 
-    Capsule::schema()->drop('widgets');
+    Capsule::schema()->drop('users');
+    Capsule::schema()->drop('posts');
 
     //echo "\033[31m some colored text \033[0m some white text \n";
 
